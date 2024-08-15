d200068f6d5e40305817ab1bb1d0afe4fc4abe1d
idr4 <andres.calderon@ikitech.com.co>
2024-08-15 10:37:41 -0500

    cm: up ca structure

---
+++ applications/app-service/src/main/java/ib/slice/order/portal/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package ib.slice.order.portal.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "ib.slice.order.portal.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
